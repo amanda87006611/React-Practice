b01ab6cc4ee4ddad7972f868615b1513894c9453
Amanda Chiu <amanda87006611@gmail.com>
2022-02-28 20:44:31 +0800

    2/28 LogInPage

--- src/data/WebApi.js
+++ src/data/WebApi.js
@@ -3,3 +3,25 @@ const BASE_URL = 'https://student-json-api.lidemy.me'
 export const getPost = (id) => {
   return fetch(`${BASE_URL}/posts?id=${id}`).then((res) => res.json())
 }
+
+export const logIn = (username, password) => {
+  return fetch(`${BASE_URL}/login`, {
+    method: 'POST',
+    headers: {
+      'content-type': 'application/json',
+    },
+    body: JSON.stringify({
+      username,
+      password,
+    }),
+  }).then((res) => res.json())
+}
+
+export const getMe = () => {
+  const token = localStorage.getItem('token')
+  return fetch(`${BASE_URL}/me`, {
+    headers: {
+      authorized: `Bearer ${token}`,
+    },
+  }).then((res) => res.json())
+}
--- src/pages/LogInPage/LogInPage.js
+++ src/pages/LogInPage/LogInPage.js
@@ -1,7 +1,56 @@
 import React, { useState, useEffect } from 'react'
 import styled from 'styled-components'
-import { HashRouter as Router, Routes, Route } from 'react-router-dom'
+import { useHistory } from 'react-router-dom'
+import { logIn } from '../../data/WebApi'
+
+const ErrorMessage = styled.div`
+  color: red;
+`
 
 export default function LogInPage() {
-  return <div>LogIn Page</div>
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const [errorMessage, setErrorMessage] = useState()
+  const history = useHistory()
+
+  const handleSubmit = (e) => {
+    setErrorMessage(null)
+    // e.preventDefault()
+    logIn(username, password).then((data) => {
+      if (data.ok === 0) {
+        setErrorMessage(data.message)
+      }
+      const token = data.token
+      localStorage.setItem('token', token)
+      // 拿到&設定token
+      history.push('/')
+      // useHistory()-> 回到首頁的路由
+    })
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <div>
+        Username:{' '}
+        <input
+          value={username}
+          onChange={(e) => {
+            return setUsername(e.target.value)
+          }}
+        />
+      </div>
+      <div>
+        Password:{' '}
+        <input
+          type="password"
+          value={password}
+          onChange={(e) => {
+            return setPassword(e.target.value)
+          }}
+        />
+      </div>
+      <button>LogIn</button>{' '}
+      {errorMessage && <ErrorMessage>{errorMessage}</ErrorMessage>}
+    </form>
+  )
 }
